6f7933d9fb092f89436262a700dce99b27015428
Jan Turk <jan.turk12@gmail.com>
2021-12-20 00:10:41 +0100

    Update networks
    Added public development and CI networks to truffle configs and yarn
    scripts.

--- hello-world/hardhat.config.js
+++ hello-world/hardhat.config.js
@@ -14,6 +14,22 @@ module.exports = {
       },
       chainId: 595,
     },
+    mandalaPubDev: {
+      url: 'http://127.0.0.1:3330',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+      },
+      chainId: 595,
+    },
+    mandalaCI: {
+      url: 'http://127.0.0.1:3330',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+      },
+      chainId: 595,
+    },
   }
 };
 
--- token/hardhat.config.js
+++ token/hardhat.config.js
@@ -13,9 +13,22 @@ module.exports = {
         path: "m/44'/60'/0'/0",
       },
       chainId: 595,
-      gasPrice: 429496729610000, // storage_limit = 100000, validUntil = 10000, 100000 << 32 | 10000
+    },
+    mandalaPubDev: {
+      url: 'http://127.0.0.1:3330',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+      },
+      chainId: 595,
+    },
+    mandalaCI: {
+      url: 'http://127.0.0.1:3330',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+      },
+      chainId: 595,
     },
   }
 };
-
-

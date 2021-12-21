a3d633b7a1bcf5ba6ecdac392d7eabe56ff9b390
Jan Turk <jan.turk12@gmail.com>
2021-12-21 01:40:46 +0100

    Minor output rewording in run.sh

---
+++ run.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+failed=0
+
+build_all() {
+  sh -c 'rush build \
+    -t hardhat-tutorial-hello-world \
+    -t hardhat-tutorial-echo \
+    -t hardhat-tutorial-token \
+    -t hardhat-tutorial-nft'
+}
+
+rebuild_all() {
+  sh -c 'rush rebuild \
+    -t hardhat-tutorial-hello-world \
+    -t hardhat-tutorial-echo \
+    -t hardhat-tutorial-token \
+    -t hardhat-tutorial-nft'
+}
+
+test_all() {
+  examples=(
+    "hello-world"
+    "echo"
+    "token"
+    "nft"
+  )
+
+  ROOT=$(pwd)
+
+  for e in "${examples[@]}"
+  do
+    echo "--------------- Testing Hardhat examples ${e} ---------------"
+
+    cd  "${ROOT}/${e}"
+
+    if [ $1 = "CI" ]; then
+      if ! yarn test-mandala:CI; then
+        ((failed=failed+1))
+      fi
+    else
+      if ! yarn test-mandala; then
+        ((failed=failed+1))
+      fi
+    fi
+
+    echo ""
+  done
+
+  echo "+++++++++++++++++++++++"
+  echo "Number of failed Hardhat tests: $failed"
+  echo "+++++++++++++++++++++++"
+}
+
+build_and_test() {
+  build_all
+  test_all $1
+
+  exit $failed
+}
+
+case "$1" in
+  "build") build_all ;;
+  "rebuild") rebuild_all ;;
+  "test") test_all "local" ;;
+  "build_and_test") build_and_test "local" ;;
+  "CI_build_and_test") build_and_test "CI" ;;
+  *) build_and_test "local" ;;
+esac

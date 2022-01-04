26e20c609121be6a93ba07f70af8d5947a7f5f85
Jan Turk <jan.turk12@gmail.com>
2022-01-04 22:16:40 +0100

    Add transaction parameters and loop helper to Token
    Added transaction parameters to deployment transactions initiated by the
    scripts of Token tutorial.
    Added loop helper to be able to run the Token tutorial in the
    --instant-sealing environments as well as scripts to trigger this
    helper.
    Updated readme to reflect the changes.

--- token/scripts/deploy.js
+++ token/scripts/deploy.js
@@ -1,4 +1,17 @@
+const { calcEthereumTransactionParams } = require("@acala-network/eth-providers");
+
+const txFeePerGas = '199999946752';
+const storageByteDeposit = '100000000000000';
+
 async function main() {
+  const ethParams = calcEthereumTransactionParams({
+    gasLimit: '2100001',
+    validUntil: '360001',
+    storageLimit: '64001',
+    txFeePerGas,
+    storageByteDeposit
+  });
+
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contract with the account:", deployer.address);
@@ -6,7 +19,13 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const Token = await ethers.getContractFactory("Token");
-  const instance = await Token.deploy(1234567890);
+  const instance = await Token.deploy(
+      1234567890,
+      {
+        gasPrice: ethParams.txGasPrice,
+        gasLimit: ethParams.txGasLimit
+      }
+    );
 
   console.log("Token address:", instance.address);
 

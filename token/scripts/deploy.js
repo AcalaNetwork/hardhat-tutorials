b15a612d67bca960cd83633c7622e6e2785f4d5e
Jan Turk <jan.turk12@gmail.com>
2022-01-07 01:08:24 +0100

    Removed gas parameters from Token tutorial
    Removed the manual setting of the gas parameters from the Token tutorial
    as it is only done to bypass the missing RPC calls and should not be
    present in the final version of the Acala EVM+.

--- token/scripts/deploy.js
+++ token/scripts/deploy.js
@@ -1,17 +1,4 @@
-const { calcEthereumTransactionParams } = require("@acala-network/eth-providers");
-
-const txFeePerGas = '199999946752';
-const storageByteDeposit = '100000000000000';
-
 async function main() {
-  const ethParams = calcEthereumTransactionParams({
-    gasLimit: '2100001',
-    validUntil: '360001',
-    storageLimit: '64001',
-    txFeePerGas,
-    storageByteDeposit
-  });
-
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contract with the account:", deployer.address);
@@ -19,13 +6,7 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const Token = await ethers.getContractFactory("Token");
-  const instance = await Token.deploy(
-      1234567890,
-      {
-        gasPrice: ethParams.txGasPrice,
-        gasLimit: ethParams.txGasLimit
-      }
-    );
+  const instance = await Token.deploy(1234567890);
 
   console.log("Token address:", instance.address);
 

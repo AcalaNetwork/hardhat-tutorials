d570dd4942f28ef169584fec82a3d4d9dc6a2617
Jan Turk <jan.turk12@gmail.com>
2022-01-07 11:52:02 +0100

    Removed gas parameters from HelloWorld tutorial
    Removed the manual setting of the gas parameters from the HelloWorld
    tutorial as it is only done to bypass the missing RPC calls and should
    not be present in the final version of the Acala EVM+.
    The tutorial in README has been updated to reflect recent changes.

--- hello-world/scripts/deploy.js
+++ hello-world/scripts/deploy.js
@@ -1,17 +1,4 @@
-const { calcEthereumTransactionParams } = require("@acala-network/eth-providers");
-
-const txFeePerGas = '199999946752';
-const storageByteDeposit = '100000000000000';
-
 async function main() {
-  const ethParams = calcEthereumTransactionParams({
-    gasLimit: '2100001',
-    validUntil: '360001',
-    storageLimit: '64001',
-    txFeePerGas,
-    storageByteDeposit
-  });
-
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
@@ -19,10 +6,7 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const HelloWorld = await ethers.getContractFactory("HelloWorld");
-  const instance = await HelloWorld.deploy({
-    gasPrice: ethParams.txGasPrice,
-    gasLimit: ethParams.txGasLimit,
-  });
+  const instance = await HelloWorld.deploy();
 
   await instance.deployed();
 

07cf865764d9536eeafecd12ed2775848227a1e7
Jan Turk <jan.turk12@gmail.com>
2022-01-14 11:58:50 +0100

    Add deploy transaction parameters to HelloWorld
    As deploy transactions need transaction parameters to be specified
    manually, they have been added. The changes are reflected in the README.

--- hello-world/test/HelloWorld.js
+++ hello-world/test/HelloWorld.js
@@ -1,10 +1,25 @@
 const { expect } = require("chai");
+const { calcEthereumTransactionParams } = require("@acala-network/eth-providers");
+
+const txFeePerGas = '199999946752';
+const storageByteDeposit = '100000000000000';
 
 describe("HelloWorld contract", async function () {
     it("returns the right value after the contract is deployed", async function () {
+        const ethParams = calcEthereumTransactionParams({
+          gasLimit: '2100001',
+          validUntil: '360001',
+          storageLimit: '64001',
+          txFeePerGas,
+          storageByteDeposit
+        });
+
         const HelloWorld = await ethers.getContractFactory("HelloWorld");
         
-        const instance = await HelloWorld.deploy();
+        const instance = await HelloWorld.deploy({
+            gasPrice: ethParams.txGasPrice,
+            gasLimit: ethParams.txGasLimit,
+        });
 
         const value = await instance.helloWorld();
 

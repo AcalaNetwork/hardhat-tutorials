5fe27806f8e391734706caf8f448dd1c2d23f408
Jan Turk <jan.turk12@gmail.com>
2022-01-16 22:48:30 +0100

    Change local Mandala port to 8545 in Echo
    Changed the network configuration for the local developlent network from
    the original 3330 port of the RPC adapter to the more traditional 8545.

--- echo/hardhat.config.js
+++ echo/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     mandala: {
-      url: 'http://127.0.0.1:3330',
+      url: 'http://127.0.0.1:8545',
       accounts: {
         mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
         path: "m/44'/60'/0'/0",

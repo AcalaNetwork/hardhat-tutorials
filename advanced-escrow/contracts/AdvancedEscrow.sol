19f3eb6c75917708bdd679f35925130294e2d134
Jan Turk <jan.turk12@gmail.com>
2022-02-09 01:07:12 +0100

    Add AdvancedEscrow smart contract
    Added AdvancedEscrow smart contract.

---
+++ advanced-escrow/contracts/AdvancedEscrow.sol
@@ -0,0 +1,147 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@acala-network/contracts/dex/IDEX.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@acala-network/contracts/schedule/ISchedule.sol";
+ 
+import "@acala-network/contracts/utils/Address.sol";
+
+contract AdvancedEscrow is ADDRESS {
+    IDEX dex = IDEX(ADDRESS.DEX);
+    ISchedule schedule = ISchedule(ADDRESS.Schedule);
+
+    uint256 public numberOfEscrows;
+
+    mapping(uint256 => Escrow) public escrows;
+
+    struct Escrow {
+        address initiator;
+        address beneficiary;
+        address ingressToken;
+        address egressToken;
+        uint256 AusdValue;
+        uint256 deadline;
+        bool completed;
+    }
+
+    constructor() {
+        numberOfEscrows = 0;
+    }
+
+    event EscrowUpdate(
+        address indexed initiator,
+        address indexed beneficiary,
+        uint256 AusdValue,
+        bool fulfilled
+    );
+
+    function initiateEscrow(
+        address beneficiary_,
+        address ingressToken_,
+        uint256 ingressValue,
+        uint256 period
+    )
+        public returns (bool)
+    {
+        // Check to make sure the latest escrow is completed
+        // Additional check is needed to ensure that the first escrow can be initiated and that the
+        // guard statement doesn't underflow
+        require(
+            numberOfEscrows == 0 || escrows[numberOfEscrows - 1].completed,
+            "Escrow: current escrow not yet completed"
+        );
+        require(beneficiary_ != address(0), "Escrow: beneficiary_ is 0x0");
+        require(ingressToken_ != address(0), "Escrow: ingressToken_ is 0x0");
+        require(period != 0, "Escrow: period is 0");
+
+        uint256 contractBalance = ERC20(ingressToken_).balanceOf(address(this));
+        require(
+            contractBalance >= ingressValue,
+            "Escrow: contract balance is less than ingress value"
+        );
+
+        ERC20 AUSDtoken = ERC20(ADDRESS.AUSD);
+        uint256 initalAusdBalance = AUSDtoken.balanceOf(address(this));
+        
+        address[] memory path = new address[](2);
+        path[0] = ingressToken_;
+        path[1] = ADDRESS.AUSD;
+        require(dex.swapWithExactSupply(path, ingressValue, 1), "Escrow: Swap failed");
+        
+        uint256 finalAusdBalance = AUSDtoken.balanceOf(address(this));
+        
+        schedule.scheduleCall(
+            address(this),
+            0,
+            1000000,
+            5000,
+            period,
+            abi.encodeWithSignature("completeEscrow()")
+        );
+
+        Escrow storage currentEscrow = escrows[numberOfEscrows];
+        currentEscrow.initiator = msg.sender;
+        currentEscrow.beneficiary = beneficiary_;
+        currentEscrow.ingressToken = ingressToken_;
+        currentEscrow.AusdValue = finalAusdBalance - initalAusdBalance;
+        currentEscrow.deadline = block.number + period;
+        numberOfEscrows += 1;
+        
+        emit EscrowUpdate(msg.sender, beneficiary_, currentEscrow.AusdValue, false);
+        
+        return true;
+    }
+
+    function setEgressToken(address egressToken_) public returns (bool) {
+        require(!escrows[numberOfEscrows - 1].completed, "Escrow: already completed");
+        require(
+            escrows[numberOfEscrows - 1].beneficiary == msg.sender,
+            "Escrow: sender is not beneficiary"
+        );
+
+        escrows[numberOfEscrows - 1].egressToken = egressToken_;
+
+        return true;
+    }
+
+    function completeEscrow() public returns (bool) {
+        Escrow storage currentEscrow = escrows[numberOfEscrows - 1];
+        require(!currentEscrow.completed, "Escrow: escrow already completed");
+        require(
+            msg.sender == currentEscrow.initiator || msg.sender == address(this),
+            "Escrow: caller is not initiator or this contract"
+        );
+
+        if(currentEscrow.egressToken != address(0)){
+            ERC20 token = ERC20(currentEscrow.egressToken);
+            uint256 initialBalance = token.balanceOf(address(this));
+            
+            address[] memory path = new address[](2);
+            path[0] = ADDRESS.AUSD;
+            path[1] = currentEscrow.egressToken;
+            require(
+                dex.swapWithExactSupply(path, currentEscrow.AusdValue, 1),
+                "Escrow: Swap failed"
+            );
+            
+            uint256 finalBalance = token.balanceOf(address(this));
+
+            token.transfer(currentEscrow.beneficiary, finalBalance - initialBalance);
+        } else {
+            ERC20 AusdToken = ERC20(ADDRESS.AUSD);
+            AusdToken.transfer(currentEscrow.beneficiary, currentEscrow.AusdValue);
+        }
+
+        currentEscrow.completed = true;
+
+        emit EscrowUpdate(
+            currentEscrow.initiator,
+            currentEscrow.beneficiary,
+            currentEscrow.AusdValue,
+            true
+        );
+
+        return true;
+    }
+}
---
+++ advanced-escrow/hardhat.config.js
@@ -0,0 +1,48 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    mandala: {
+      url: 'http://127.0.0.1:8545',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+      },
+      chainId: 595,
+    },
+    mandalaPubDev: {
+      url: 'https://tc7-eth.aca-dev.network',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+      },
+      chainId: 595,
+      timeout: 60000,
+    },
+    mandalaCI: {
+      url: 'http://eth-rpc-adapter-server:8545',
+      accounts: {
+        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
+        path: "m/44'/60'/0'/0",
+      },
+      chainId: 595,
+    },
+  }
+};

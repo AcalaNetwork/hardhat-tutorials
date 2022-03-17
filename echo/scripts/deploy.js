5b2836ffe9fa8fb507cd7639cea386d44553852e
Jan Turk <jan.turk12@gmail.com>
2022-03-17 22:44:31 +0100

    Add transactionHelper to Echo
    Deploy transaction parameter calculation was extracted from the deploy
    script into a self standing utility. This simplifies the deploy scripts.

--- echo/scripts/deploy.js
+++ echo/scripts/deploy.js
@@ -1,18 +1,7 @@
-const { calcEthereumTransactionParams } = require("@acala-network/eth-providers");
-
-const txFeePerGas = '199999946752';
-const storageByteDeposit = '100000000000000';
+const { txParams } = require("../utils/transactionHelper");
 
 async function main() {
-  const blockNumber = await ethers.provider.getBlockNumber();
-
-  const ethParams = calcEthereumTransactionParams({
-    gasLimit: '2100001',
-    validUntil: (blockNumber + 100).toString(),
-    storageLimit: '64001',
-    txFeePerGas,
-    storageByteDeposit
-  });
+  const ethParams = await txParams();
 
   const [deployer] = await ethers.getSigners();
 

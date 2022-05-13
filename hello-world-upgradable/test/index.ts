14dbd33a8ab53a546189089e510586471c20d9cf
Shunji Zhan <shunhahaha@live.com>
2022-05-13 12:17:17 +0800

    update

--- hello-world-upgradable/test/index.ts
+++ hello-world-upgradable/test/index.ts
@@ -6,10 +6,22 @@ describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const provider = EvmRpcProvider.from("ws://localhost:9944");
     await provider.isReady();
+
+    /* ----------
+      some examples might use `calcEthereumTransactionParams(...args)`
+      which internally calls _getEthGas(...args)
+      so we can also use _getEthGas() directly here
+                                                            ---------- */
+    const gasPriceOverrides = (await provider._getEthGas()).gasPrice;
+
+    /* ----------
+      a workaround to force overriding gasPrice
+      the provider will auto calculate a valid gasLimit with this gasPrice 
+                                                                ---------- */
     provider.getFeeData = async () => ({
       maxFeePerGas: null,
       maxPriorityFeePerGas: null,
-      gasPrice: (await provider._getEthGas()).gasPrice,
+      gasPrice: gasPriceOverrides,
     });
 
     const signer = ethers.Wallet.fromMnemonic(
@@ -17,16 +29,14 @@ describe("Greeter", function () {
     ).connect(provider);
 
     const Greeter = await ethers.getContractFactory("Greeter", signer);
-    const greeter = await upgrades.deployProxy(Greeter, ["Hello, Hardhat!"]);
+    const greeter = await upgrades.deployProxy(Greeter, ["Hello, world!"]);
 
-    // const res = greeter.greet()
     expect(await greeter.greet()).to.equal("Hello, world!");
 
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
+    await setGreetingTx.wait();
 
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });

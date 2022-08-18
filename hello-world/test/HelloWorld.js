e70ff101d3c7182bb5b76f94f2308afc4a05966a
Jan Turk <jan.turk12@gmail.com>
2022-08-18 10:24:05 +0200

    Add -r mode to HelloWorld
    Added support for -r flag in the RPC adapter in the HelloWorld test.
    This also makes them compatible with the Hardhat's emulated network once
    again.

--- hello-world/test/HelloWorld.js
+++ hello-world/test/HelloWorld.js
@@ -1,27 +1,10 @@
 const { expect } = require('chai');
-const { calcEthereumTransactionParams } = require('@acala-network/eth-providers');
-
-const txFeePerGas = '199999946752';
-const storageByteDeposit = '100000000000000';
 
 describe('HelloWorld contract', function () {
   it('returns the right value after the contract is deployed', async function () {
-    const blockNumber = await ethers.provider.getBlockNumber();
-
-    const ethParams = calcEthereumTransactionParams({
-      gasLimit: '2100001',
-      validUntil: (blockNumber + 100).toString(),
-      storageLimit: '64001',
-      txFeePerGas,
-      storageByteDeposit
-    });
-
     const HelloWorld = await ethers.getContractFactory('HelloWorld');
 
-    const instance = await HelloWorld.deploy({
-      gasPrice: ethParams.txGasPrice,
-      gasLimit: ethParams.txGasLimit
-    });
+    const instance = await HelloWorld.deploy();
 
     const value = await instance.helloWorld();
 

838ce238e7ea9fc5c9f32978375f525326d40661
Shunji Zhan <shunhahaha@live.com>
2023-05-31 12:34:18 +0800

    hello world

--- hello-world/scripts/deploy.ts
+++ hello-world/scripts/deploy.ts
@@ -1,26 +1,16 @@
-const { txParams } = require('../utils/transactionHelper');
+import { ethers } from "hardhat";
 
 async function main() {
-  const ethParams = await txParams();
-
   const [deployer] = await ethers.getSigners();
-
   console.log('Deploying contracts with the account:', deployer.address);
-
   console.log('Account balance:', (await deployer.getBalance()).toString());
 
   const HelloWorld = await ethers.getContractFactory('HelloWorld');
-  const instance = await HelloWorld.deploy({
-    gasPrice: ethParams.txGasPrice,
-    gasLimit: ethParams.txGasLimit
-  });
-
+  const instance = await HelloWorld.deploy();
   await instance.deployed();
-
   console.log('HelloWorld address:', instance.address);
 
   const value = await instance.helloWorld();
-
   console.log('Stored value:', value);
 }
 
--- hello-world/test/hello-world.ts
+++ hello-world/test/hello-world.ts
@@ -1,13 +1,13 @@
-const { expect } = require('chai');
+import { ethers } from "hardhat";
+import { expect } from "chai";
 
 describe('HelloWorld contract', function () {
   it('returns the right value after the contract is deployed', async function () {
     const HelloWorld = await ethers.getContractFactory('HelloWorld');
-
     const instance = await HelloWorld.deploy();
+    await instance.deployed();
 
     const value = await instance.helloWorld();
-
     expect(value).to.equal('Hello World!');
   });
 });
